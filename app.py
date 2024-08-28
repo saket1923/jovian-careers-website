9590d157f5a1fb98e8482c29be7ca983681cab43
saket1923 <158184987+saket1923@users.noreply.github.com>
2024-08-28 15:25:41 +0000

    added dynamic data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
 
 app = Flask(__name__)
 JOBS=[
@@ -33,6 +33,11 @@ def index():
     return render_template("home.html",
                            jobs=JOBS,
                           company_name="Jovian ")
+
+
+@app.route('/api/JOBS')
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",debug=True)
